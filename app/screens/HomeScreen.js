8fd22950beb25c62e96f668f4f358e7c7ba13102
jeyasurya2126 <jeyasurya.muthuraman@crestclimbers.com>
2025-09-06 13:56:40 +0530

    Initial Commit

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to RideIn 🚖</Text>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  title: { fontSize: 20, fontWeight: 'bold' },
+});
